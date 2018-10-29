4b59278f168889715648e29602b315134b90d219
yisus23 <choyn4@gmail.com>
2018-10-29 12:15:16 -0500

    primera version de dialogos

---
+++ app/src/main/java/mx/edu/itchetumal/dadm/dialogos/AlertDialog.java
@@ -0,0 +1,41 @@
+package mx.edu.itchetumal.dadm.dialogos;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+
+public class AlertaDialogo extends DialogFragment {
+
+    Button btn1;
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        btn1 = (Button) btn1.findViewById(R.id.button);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setMessage("Esto es un dialogo").setTitle("Dialogo").setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                builder.create();
+            }
+        });
+
+
+
+
+    }
+
+
+
+}
---
+++ app/src/main/java/mx/edu/itchetumal/dadm/dialogos/MainActivity.java
@@ -0,0 +1,24 @@
+package mx.edu.itchetumal.dadm.dialogos;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+    }
+}

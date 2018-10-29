262566e34530120dc115cbb30f4813dade6fdb50
yisus23 <choyn4@gmail.com>
2018-10-29 12:16:49 -0500

    primera version 1.1

--- app/src/main/java/mx/edu/itchetumal/dadm/dialogos/AlertDialog.java
+++ app/src/main/java/mx/edu/itchetumal/dadm/dialogos/AlertDialog.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AlertDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.support.v7.app.AppCompatActivity;
 
 
 
